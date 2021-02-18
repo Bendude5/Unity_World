9964329340464fbb8e251e5f8e33214c3d038065
Bendude5 <Ben.miller@mail.bcu.ac.uk>
2021-02-18 19:57:03 +0000

    Unity White box 1.14

---
+++ White box/Assets/Scripts/Camera_Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+    public Camera Cam1;
+    public Camera Cam2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam1.enabled = true;
+        Cam2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.T))
+        {
+            Cam1.enabled = false;
+            Cam2.enabled = true;
+        }
+    }
+}
